77f9ef01cb5ce9d4db14318e2baf064856907549
Jpegzilla <jpegzilla@gmail.com>
2019-04-16 17:32:48 -0400

    function to parse .mkz file to array of html elements

--- js/markzilla.js
+++ js/markzilla.js
@@ -34,90 +34,145 @@ const parseDateString = (str) => {
 const markzilla = mkz = {
   parse: (filePath) => {
     $.get(filePath, (data) => {
-      console.log(data);
       inputFile = data;
     }).then((inputFile) => {
       let lines = inputFile.split('\n');
-      let lastLine;
-      let line;
-      let elements = [];
-      console.log(elements);
+      let arrayOfLines = [];
       for (var i = 0; i < lines.length; i++) {
-        // console.log(lastLine);
-        let str = lines[i].split(' ')[0];
-        if (lastLine) {
-
+        if (/^#/.test(lines[i])) {
+          let lastIndicator = lines[i];
+          arrayOfLines.push(
+            lastIndicator
+          );
+          continue;
         }
-        switch (true) {
-          case (/\s/.test(str) || str == ''):
-            console.log('blank line');
-            break;
+        if (/\n\r/.test(lines[i])) {
+          continue;
+        }
+        if (/^\s/.test(lines[i]) || lines[i] == '') {
+          continue;
+        }
+        if (/^\/\//.test(lines[i])) {
+          continue;
+        } else {
+          arrayOfLines[(arrayOfLines.length - 1)] = arrayOfLines[(arrayOfLines.length - 1)] += ('<br>' + lines[i]);
+          continue;
+        }
+      }
 
-          case str == '#p':
-            line = lines[i].split('#p ');
-            line = '<p>' + line[1] + '</p>';
+      let elements = [];
+      for (var i = 0; i < arrayOfLines.length; i++) {
+        lineText = arrayOfLines[i].split(/^#[a-z]*[0-9]*\s/)[1];
+        lineType = arrayOfLines[i].split(/\s/)[0].split('#')[1];
+        line = arrayOfLines[i];
+
+        switch (lineType) {
+          case 'h1':
+          case 'h2':
+          case 'h3':
+          case 'h4':
+          case 'h5':
+          case 'h6':
+          case 'p':
+          case 'code':
+            c = `<${lineType}>${lineText}</${lineType}>`;
+            elements.push(c);
             break;
-          case str == '//':
-            break;
-          case str == '#h1':
-            line = lines[i].split('#h1 ');
-            line = '<h1>' + line[1] + '</h1>';
-            elements.push({
-              line
-            });
-            break;
-          case str == '#h2':
-            line = lines[i].split('#h2 ');
-            line = '<h2>' + line[1] + '</h2>';
-            elements.push({
-              line
-            });
-            break;
-          case str == '#h3':
-            line = lines[i].split('#h3 ');
-            line = '<h3>' + line[1] + '</h3>';
-            elements.push({
-              line
-            });
-            break;
-          case str == '#h4':
-            line = lines[i].split('#h4 ');
-            line = '<h4>' + line[1] + '</h4>';
-            elements.push({
-              line
-            });
-            break;
-          case str == '#h5':
-            line = lines[i].split('#h5 ');
-            line = '<h5>' + line[1] + '</h5>';
-            elements.push({
-              line
-            });
-            break;
-          case str == '#h6':
-            line = lines[i].split('#h6 ');
-            line = '<h6>' + line[1] + '</h6>';
-            elements.push({
-              line
-            });
-            break;
-          case str == '#img':
-            line = lines[i].split('#h1 ');
-            line = '<img src=\'' + line[1] + '\'/>';
-            elements.push({
-              line
-            });
+          case 'img':
+            x = lineText.split(/(^.*\/\S*)/);
+            imgPath = x[1];
+            imgAlt = x[2].trim();
+            c = `<${lineType} src="${imgPath}" alt="${imgAlt}">`;
+            elements.push(c);
             break;
           default:
-            // if ((lastLine !== '') || (/\s/.test(str) == false)) {
-            //   console.log('last line was not blank, and had no markup information associated with it.');
-            // } else {
-            //   console.log('last line was blank, and had no markup information associated with it.');
-            // }
-            break;
-        } // end switch
-        lastLine = lines[i];
+
+        }
+
       } // end loop
+      // const matchesHL = /(?:[^\'|\"])(\[.*?\])(?:[^\'|\"])/gi;
+      const matchesHL = /(\[[^\\a].*?\])/gi;
+      const matchesLinks = /(\[[a].*?\]\(.*?\))/gi;
+      const matchesEsc = /(\[\\)/gi;
+      const matchesLinkText = /(\[[a].*?\])/;
+      const matchesLinkAdd = /(\(.*?\))/;
+      for (var i = 0; i < elements.length; i++) {
+
+        let hl = elements[i].match(matchesHL);
+        let li = elements[i].match(matchesLinks);
+
+        if (li && hl) {
+          let line = elements[i];
+          let finalLine;
+          el = elements[i].match(matchesHL);
+          for (var c = 0; c < el.length; c++) {
+            currentHL = el[c];
+            elPosition = elements[i].indexOf(el[c]);
+            elType = el[c].replace(/(\[|\])/gi, '').substr(0, el[c].replace(/(\[|\])/gi, '').indexOf(' '));
+            elType = elType.split('').join(' ');
+            elContent = el[c].replace(/(\[|\])/gi, '').substr(el[c].replace(/(\[|\])/gi, '').indexOf(' ') + 1);
+            let lineHTML = line.substr(elPosition, currentHL.length);
+            elToLast = (currentHL.length + elPosition);
+            lineLength = line.length;
+            hLlength = currentHL.length;
+            finalLine = elements[i].substr(0, elPosition) + `<span class='${elType}'>${elContent}</span>` + elements[i].substr(elToLast, lineLength);
+            elements[i] = finalLine;
+          }
+
+          el = elements[i].match(matchesLinks);
+          for (var g = 0; g < el.length; g++) {
+            line = elements[i];
+            lineLength = line.length;
+            elPosition = elements[i].indexOf(el[g]);
+            currentLink = el[g];
+            linkText = currentLink.match(matchesLinkText)[0].replace(/(\[|\])/gi, '');
+            linkAdd = currentLink.match(matchesLinkAdd)[0].replace(/(\(|\))/gi, '');
+            elToLast = (currentLink.length + elPosition);
+            finalLine = elements[i].substr(0, elPosition) + `<a target="_blank" href="${linkAdd}">${linkText}</a>` + elements[i].substr(elToLast, lineLength);
+            elements[i] = finalLine;
+          }
+        } else if (hl) {
+
+
+          let line = elements[i];
+          let finalLine;
+          el = elements[i].match(matchesHL);
+          for (var c = 0; c < el.length; c++) {
+            currentHL = el[c];
+            elPosition = elements[i].indexOf(el[c]);
+            elType = el[c].replace(/(\[|\])/gi, '').substr(0, el[c].replace(/(\[|\])/gi, '').indexOf(' '));
+            elType = elType.split('').join(' ');
+            elContent = el[c].replace(/(\[|\])/gi, '').substr(el[c].replace(/(\[|\])/gi, '').indexOf(' ') + 1);
+            let lineHTML = line.substr(elPosition, currentHL.length);
+            elToLast = (currentHL.length + elPosition);
+            lineLength = line.length;
+            hLlength = currentHL.length;
+            finalLine = elements[i].substr(0, elPosition) + `<span class='${elType}'>${elContent}</span>` + elements[i].substr(elToLast, lineLength);
+            elements[i] = finalLine;
+          }
+        } else if (li) {
+
+          el = elements[i].match(matchesLinks);
+          for (var g = 0; g < el.length; g++) {
+            line = elements[i];
+            lineLength = line.length;
+            elPosition = elements[i].indexOf(el[g]);
+            currentLink = el[g];
+            linkText = currentLink.match(matchesLinkText)[0].replace(/(\[|\])/gi, '');
+            linkAdd = currentLink.match(matchesLinkAdd)[0].replace(/(\(|\))/gi, '');
+            elToLast = (currentLink.length + elPosition);
+            finalLine = elements[i].substr(0, elPosition) + `<a target="_blank" href="${linkAdd}">${linkText}</a>` + elements[i].substr(elToLast, lineLength);
+            elements[i] = finalLine;
+          }
+        }
+      }
+
+      for (var i = 0; i < elements.length; i++) {
+        elements[i] = elements[i].replace(matchesEsc, '[');
+      }
+      console.log(elements);
+      return;
+
 
     });
   },

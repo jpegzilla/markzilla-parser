645410e0e1ad121159185d87199c3f6b38fffb5b
Jpegzilla <jpegzilla@gmail.com>
2019-04-17 04:24:22 -0400

    parser v1 complete. vanilla css theme and html for testing now included

---
+++ README.md
@@ -0,0 +1,25 @@
+## what is this?
+this is a parser for a markup 'language' I created called markzilla. I did it mainly as a learning exercise -- I learned everything I'll ever need to know about regex, for example. but if you need a simple markup language that outputs a JSON object full of HTML elements (and who doesn't?) then this project may be useful to you.
+
+### how markzilla markup (.mkz) syntax works
+markzilla has a syntax designed specifically around writing articles/posts/documents that will need to be rendered as HTML. therefore all the markzilla *markup indicators* are based on HTML elements.
+
+the line indicators as of right now are #p, #h1-#h6, #img, and #code. these are the only ones I needed originally which is why there are so few. however I'll add more if there seems to be a reason. they can't be combined as of right now, but I don't think they need to be.
+
+the style indicators, which do things like make text italic and stuff, are s(trikethrough), i(talic), b(old), u(nderline), c(ode), o(verline), and d(rop-cap). these can be combined, of course.
+be careful to only place one character in a drop-cap indicator, otherwise it becomes more of a...drop-word.
+
+the methods of using these indicators are demonstrated in `test.mkz`.
+
+### functionality
+every method you need for parsing markzilla and recieving the output is in the `markzilla` / `mkz` object. for example, get the path to your `.mkz` file, and call mkz like this: `mkz.parse('path/to/file')`
+and it will give you an array of numbered JSON objects containing the parsed `.mkz` lines. running `mkz.parse()` using my test file as input converts a line that looks like this:
+
+`#h1 [i This] is a [b markzilla] test file. This is the [sui first ever] [ib markzilla] file.`
+
+to a line that looks like this.
+
+`<h1><span class='i'>This</span> is a <span class='b'>markzilla</span> test file. This is the <span class='s u i'>first ever</span> <span class='i b'>markzilla</span> file.
+</h1>`
+
+it's a pretty straightforward `.mkz` to `.html` conversion. if for some reason you *actually* want to use this, like to make blog posts on a static site or something, please feel free. and if you need help or want me to put in a new feature, please get in touch. I'm online almost 24/7 these days.
--- js/markzilla.js
+++ js/markzilla.js
@@ -2,6 +2,7 @@ let jsonObject;
 let inputFile;
 let filePath = '/../mkz/test.mkz';
 let postObject = {};
+let elements = [];
 
 const yyyymmdd = () => {
   var x = new Date();
@@ -33,153 +34,209 @@ const parseDateString = (str) => {
 
 const markzilla = mkz = {
   parse: (filePath) => {
-    $.get(filePath, (data) => {
-      inputFile = data;
-    }).then((inputFile) => {
-      let lines = inputFile.split('\n');
-      let arrayOfLines = [];
-      for (var i = 0; i < lines.length; i++) {
-        if (/^#/.test(lines[i])) {
-          let lastIndicator = lines[i];
-          arrayOfLines.push(
-            lastIndicator
-          );
-          continue;
-        }
-        if (/\n\r/.test(lines[i])) {
-          continue;
-        }
-        if (/^\s/.test(lines[i]) || lines[i] == '') {
-          continue;
-        }
-        if (/^\/\//.test(lines[i])) {
-          continue;
-        } else {
-          arrayOfLines[(arrayOfLines.length - 1)] = arrayOfLines[(arrayOfLines.length - 1)] += ('<br>' + lines[i]);
-          continue;
-        }
-      }
-
-      let elements = [];
-      for (var i = 0; i < arrayOfLines.length; i++) {
-        lineText = arrayOfLines[i].split(/^#[a-z]*[0-9]*\s/)[1];
-        lineType = arrayOfLines[i].split(/\s/)[0].split('#')[1];
-        line = arrayOfLines[i];
-
-        switch (lineType) {
-          case 'h1':
-          case 'h2':
-          case 'h3':
-          case 'h4':
-          case 'h5':
-          case 'h6':
-          case 'p':
-          case 'code':
-            c = `<${lineType}>${lineText}</${lineType}>`;
-            elements.push(c);
-            break;
-          case 'img':
-            x = lineText.split(/(^.*\/\S*)/);
-            imgPath = x[1];
-            imgAlt = x[2].trim();
-            c = `<${lineType} src="${imgPath}" alt="${imgAlt}">`;
-            elements.push(c);
-            break;
-          default:
-
-        }
-
-      } // end loop
-      // const matchesHL = /(?:[^\'|\"])(\[.*?\])(?:[^\'|\"])/gi;
-      const matchesHL = /(\[[^\\a].*?\])/gi;
-      const matchesLinks = /(\[[a].*?\]\(.*?\))/gi;
-      const matchesEsc = /(\[\\)/gi;
-      const matchesLinkText = /(\[[a].*?\])/;
-      const matchesLinkAdd = /(\(.*?\))/;
-      for (var i = 0; i < elements.length; i++) {
-
-        let hl = elements[i].match(matchesHL);
-        let li = elements[i].match(matchesLinks);
-
-        if (li && hl) {
-          let line = elements[i];
-          let finalLine;
-          el = elements[i].match(matchesHL);
-          for (var c = 0; c < el.length; c++) {
-            currentHL = el[c];
-            elPosition = elements[i].indexOf(el[c]);
-            elType = el[c].replace(/(\[|\])/gi, '').substr(0, el[c].replace(/(\[|\])/gi, '').indexOf(' '));
-            elType = elType.split('').join(' ');
-            elContent = el[c].replace(/(\[|\])/gi, '').substr(el[c].replace(/(\[|\])/gi, '').indexOf(' ') + 1);
-            let lineHTML = line.substr(elPosition, currentHL.length);
-            elToLast = (currentHL.length + elPosition);
-            lineLength = line.length;
-            hLlength = currentHL.length;
-            finalLine = elements[i].substr(0, elPosition) + `<span class='${elType}'>${elContent}</span>` + elements[i].substr(elToLast, lineLength);
-            elements[i] = finalLine;
+    if (filePath) {
+      elements = [];
+      $.get(filePath, (data) => {
+        inputFile = data;
+      }).then((inputFile) => {
+        let lines = inputFile.split('\n');
+        let arrayOfLines = [];
+        for (var i = 0; i < lines.length; i++) {
+          if (/^#/.test(lines[i])) {
+            let lastIndicator = lines[i];
+            arrayOfLines.push(
+              lastIndicator
+            );
+            continue;
           }
-
-          el = elements[i].match(matchesLinks);
-          for (var g = 0; g < el.length; g++) {
-            line = elements[i];
-            lineLength = line.length;
-            elPosition = elements[i].indexOf(el[g]);
-            currentLink = el[g];
-            linkText = currentLink.match(matchesLinkText)[0].replace(/(\[|\])/gi, '');
-            linkAdd = currentLink.match(matchesLinkAdd)[0].replace(/(\(|\))/gi, '');
-            elToLast = (currentLink.length + elPosition);
-            finalLine = elements[i].substr(0, elPosition) + `<a target="_blank" href="${linkAdd}">${linkText}</a>` + elements[i].substr(elToLast, lineLength);
-            elements[i] = finalLine;
+          if (/\n\r/.test(lines[i])) {
+            continue;
           }
-        } else if (hl) {
-
-
-          let line = elements[i];
-          let finalLine;
-          el = elements[i].match(matchesHL);
-          for (var c = 0; c < el.length; c++) {
-            currentHL = el[c];
-            elPosition = elements[i].indexOf(el[c]);
-            elType = el[c].replace(/(\[|\])/gi, '').substr(0, el[c].replace(/(\[|\])/gi, '').indexOf(' '));
-            elType = elType.split('').join(' ');
-            elContent = el[c].replace(/(\[|\])/gi, '').substr(el[c].replace(/(\[|\])/gi, '').indexOf(' ') + 1);
-            let lineHTML = line.substr(elPosition, currentHL.length);
-            elToLast = (currentHL.length + elPosition);
-            lineLength = line.length;
-            hLlength = currentHL.length;
-            finalLine = elements[i].substr(0, elPosition) + `<span class='${elType}'>${elContent}</span>` + elements[i].substr(elToLast, lineLength);
-            elements[i] = finalLine;
+          if (/^\s/.test(lines[i]) || lines[i] == '') {
+            continue;
           }
-        } else if (li) {
-
-          el = elements[i].match(matchesLinks);
-          for (var g = 0; g < el.length; g++) {
-            line = elements[i];
-            lineLength = line.length;
-            elPosition = elements[i].indexOf(el[g]);
-            currentLink = el[g];
-            linkText = currentLink.match(matchesLinkText)[0].replace(/(\[|\])/gi, '');
-            linkAdd = currentLink.match(matchesLinkAdd)[0].replace(/(\(|\))/gi, '');
-            elToLast = (currentLink.length + elPosition);
-            finalLine = elements[i].substr(0, elPosition) + `<a target="_blank" href="${linkAdd}">${linkText}</a>` + elements[i].substr(elToLast, lineLength);
-            elements[i] = finalLine;
+          if (/^\/\//.test(lines[i])) {
+            continue;
+          } else {
+            arrayOfLines[(arrayOfLines.length - 1)] = arrayOfLines[(arrayOfLines.length - 1)] += ('<br>' + lines[i]);
+            continue;
           }
         }
-      }
+        for (var i = 0; i < arrayOfLines.length; i++) {
+          lineText = arrayOfLines[i].split(/^#[a-z]*[0-9]*\s/)[1];
+          lineType = arrayOfLines[i].split(/\s/)[0].split('#')[1];
+          line = arrayOfLines[i];
 
-      for (var i = 0; i < elements.length; i++) {
-        elements[i] = elements[i].replace(matchesEsc, '[');
-      }
-      console.log(elements);
-      return;
+          switch (lineType) {
+            case 'h1':
+            case 'h2':
+            case 'h3':
+            case 'h4':
+            case 'h5':
+            case 'h6':
+            case 'p':
+            case 'code':
+              c = `<${lineType}>${lineText}</${lineType}>`;
+              elements.push(c);
+              break;
+            case 'img':
+              x = lineText.split(/(^.*\/\S*)/);
+              imgPath = x[1];
+              imgAlt = x[2].trim();
+              c = `<${lineType} src="${imgPath}" alt="${imgAlt}" title="${imgAlt}">`;
+              elements.push(c);
+              break;
+            default:
+
+          }
+
+        } // end loop
+        // const matchesHL = /(?:[^\'|\"])(\[.*?\])(?:[^\'|\"])/gi;
+        const matchesHL = /(\[[^\\a].*?\])/gi;
+        const matchesLinks = /(\[[a].*?\]\(.*?\))/gi;
+        const matchesEsc = /(\[\\)/gi;
+        const matchesLinkText = /(\[[a].*?\])/;
+        const matchesLinkAdd = /(\(.*?\))/;
+        for (var i = 0; i < elements.length; i++) {
+
+          let hl = elements[i].match(matchesHL);
+          let li = elements[i].match(matchesLinks);
+
+          if (li && hl) {
+            let line = elements[i];
+            let finalLine;
+            el = elements[i].match(matchesHL);
+            for (var c = 0; c < el.length; c++) {
+              currentHL = el[c];
+              elPosition = elements[i].indexOf(el[c]);
+              elType = el[c].replace(/(\[|\])/gi, '').substr(0, el[c].replace(/(\[|\])/gi, '').indexOf(' '));
+              elType = elType.split('').join(' ');
+              elContent = el[c].replace(/(\[|\])/gi, '').substr(el[c].replace(/(\[|\])/gi, '').indexOf(' ') + 1);
+              let lineHTML = line.substr(elPosition, currentHL.length);
+              elToLast = (currentHL.length + elPosition);
+              lineLength = line.length;
+              hLlength = currentHL.length;
+              finalLine = elements[i].substr(0, elPosition) + `<span class='${elType}'>${elContent}</span>` + elements[i].substr(elToLast, lineLength);
+              elements[i] = finalLine;
+            }
+
+            el = elements[i].match(matchesLinks);
+            for (var g = 0; g < el.length; g++) {
+              line = elements[i];
+              lineLength = line.length;
+              elPosition = elements[i].indexOf(el[g]);
+              currentLink = el[g];
+              linkText = currentLink.match(matchesLinkText)[0].replace(/(\[a\s|\])/gi, '');
+              linkAdd = currentLink.match(matchesLinkAdd)[0].replace(/(\(|\))/gi, '');
+              elToLast = (currentLink.length + elPosition);
+              finalLine = elements[i].substr(0, elPosition) + `<a target="_blank" href="${linkAdd}">${linkText}</a>` + elements[i].substr(elToLast, lineLength);
+              elements[i] = finalLine;
+            }
+          } else if (hl) {
 
 
-    });
+            let line = elements[i];
+            let finalLine;
+            el = elements[i].match(matchesHL);
+            for (var c = 0; c < el.length; c++) {
+              currentHL = el[c];
+              elPosition = elements[i].indexOf(el[c]);
+              elType = el[c].replace(/(\[|\])/gi, '').substr(0, el[c].replace(/(\[|\])/gi, '').indexOf(' '));
+              elType = elType.split('').join(' ');
+              elContent = el[c].replace(/(\[|\])/gi, '').substr(el[c].replace(/(\[|\])/gi, '').indexOf(' ') + 1);
+              let lineHTML = line.substr(elPosition, currentHL.length);
+              elToLast = (currentHL.length + elPosition);
+              lineLength = line.length;
+              hLlength = currentHL.length;
+              finalLine = elements[i].substr(0, elPosition) + `<span class='${elType}'>${elContent}</span>` + elements[i].substr(elToLast, lineLength);
+              elements[i] = finalLine;
+            }
+          } else if (li) {
+
+            el = elements[i].match(matchesLinks);
+            for (var g = 0; g < el.length; g++) {
+              line = elements[i];
+              lineLength = line.length;
+              elPosition = elements[i].indexOf(el[g]);
+              currentLink = el[g];
+              linkText = currentLink.match(matchesLinkText)[0].replace(/(\[a\s|\])/gi, '');
+              linkAdd = currentLink.match(matchesLinkAdd)[0].replace(/(\(|\))/gi, '');
+              console.log(linkText);
+              elToLast = (currentLink.length + elPosition);
+              finalLine = elements[i].substr(0, elPosition) + `<a target="_blank" href="${linkAdd}">${linkText}</a>` + elements[i].substr(elToLast, lineLength);
+              elements[i] = finalLine;
+            }
+          }
+        }
+
+        for (var i = 0; i < elements.length; i++) {
+          elements[i] = elements[i].replace(matchesEsc, '[');
+        }
+
+        for (var i = 0; i < elements.length; i++) {
+          element = elements[i];
+          index = i;
+          elements[i] = {
+            [index]: element
+          };
+        }
+      });
+      return elements;
+    } else {
+      console.warn('no file path specified.');
+      return false;
+    }
   },
+
   decon: (html) => {
     console.log('converting from .html to .mkz');
+    // why would I need this though?
   },
-  save: (json) => {
-    console.log('saving as json object.');
+
+  insertPost: (postObject) => {
+    if (Object.keys(postObject)[0]) {
+      var start = window.performance.now();
+      let title = Object.values(postObject)[0].title;
+      let date = Object.keys(postObject)[0];
+      let body = Object.values(postObject)[0].body;
+      let parsedDate = parseDateString(date);
+      $('.mkzTitle').html(title)
+      $('.mkzDate').text(parsedDate.y + " " + parsedDate.m + " " + parsedDate.d + " ");
+      for (var i = 0; i < body.length; i++) {
+        if (body[i]) {
+          $('.mkzBody').append(body[i]);
+        } else {
+          continue;
+        }
+      }
+      var end = window.performance.now();
+      var time = 'completed task in ' + (Math.floor(end - start) * 0.001) + ' seconds.';
+      return time;
+    } else {
+      console.warn('no postObject defined');
+      return false;
+    }
+
+  },
+
+  saveToPost: (json) => {
+    if (json.length !== 0) {
+      console.log('saving as json post.');
+      articleDate = yyyymmdd().yyyymmdd;
+      articleTitle = Object.values(json[0])[0];
+      postObject = {
+        [articleDate]: {
+          "title": articleTitle,
+          "body": []
+        }
+      }
+      for (var i = 1; i < json.length; i++) {
+        postObject[articleDate].body[i] = Object.values(json[i])[0]
+      }
+      return postObject;
+    } else {
+      console.warn('no json to save.');
+      return false;
+    }
   }
 }

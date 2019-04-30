81b0bba55f3da5be665e7f3a3fb164174fc59843
Jpegzilla <jpegzilla@gmail.com>
2019-04-30 04:42:15 -0400

    uses promises now

--- js/markzilla.js
+++ js/markzilla.js
@@ -1,4 +1,6 @@
 let filePath = mkzOptions.file;
+let stylePath = mkzOptions.stylePath;
+let styleName = mkzOptions.styleName;
 let postObject = {};
 let elements = [];
 
@@ -58,10 +60,10 @@ const getFile = filePath => {
   });
 };
 
-getFile("/../json/styles.json").then(response => {
+getFile(stylePath).then(response => {
   let style = document.createElement("style");
   let resp = JSON.parse(response);
-  stylesheet = resp.styles.find(e => Object.keys(e) == mkzOptions.style);
+  stylesheet = resp.styles.find(e => Object.keys(e)[0] == styleName);
   stylesheet = Object.values(stylesheet)[0];
   style.innerHTML = stylesheet;
   document.body.appendChild(style);
@@ -69,177 +71,180 @@ getFile("/../json/styles.json").then(response => {
 
 const markzilla = (mkz = {
   parse: filePath => {
-    if (filePath) {
-      elements = [];
-      let inputFile;
-      getFile(filePath).then(inputFile => {
-        let lines = inputFile.split("\n");
-        let arrayOfLines = [];
-        for (var i = 0; i < lines.length; i++) {
-          if (/^#/.test(lines[i])) {
-            let lastIndicator = lines[i];
-            arrayOfLines.push(lastIndicator);
-            continue;
-          }
-          if (/\n\r/.test(lines[i])) {
-            continue;
-          }
-          if (/^\s/.test(lines[i]) || lines[i] == "") {
-            continue;
-          }
-          if (/^\/\//.test(lines[i])) {
-            continue;
-          } else {
-            arrayOfLines[arrayOfLines.length - 1] = arrayOfLines[
-              arrayOfLines.length - 1
-            ] += "<br>" + lines[i];
-            continue;
-          }
-        }
-        for (var i = 0; i < arrayOfLines.length; i++) {
-          lineText = arrayOfLines[i].split(/^#[a-z]*[0-9]*\s/)[1];
-          lineType = arrayOfLines[i].split(/\s/)[0].split("#")[1];
-          line = arrayOfLines[i];
-
-          switch (lineType) {
-            case "h1":
-            case "h2":
-            case "h3":
-            case "h4":
-            case "h5":
-            case "h6":
-            case "p":
-            case "code":
-              c = `<${lineType}>${lineText}</${lineType}>`;
-              elements.push(c);
-              break;
-            case "img":
-              x = lineText.split(/(^.*\/\S*)/);
-              imgPath = x[1];
-              imgAlt = x[2].trim();
-              c = `<${lineType} src="${imgPath}" alt="${imgAlt}" title="${imgAlt}">`;
-              elements.push(c);
-              break;
-            default:
-          }
-        }
-        const matchesHL = /(\[[^\\a].*?\])/gi;
-        const matchesLinks = /(\[[a].*?\]\(.*?\))/gi;
-        const matchesEsc = /(\[\\)/gi;
-        const matchesLinkText = /(\[[a].*?\])/;
-        const matchesLinkAdd = /(\(.*?\))/;
-        for (var i = 0; i < elements.length; i++) {
-          let hl = elements[i].match(matchesHL);
-          let li = elements[i].match(matchesLinks);
-
-          if (li && hl) {
-            let line = elements[i];
-            let finalLine;
-            el = elements[i].match(matchesHL);
-            for (var c = 0; c < el.length; c++) {
-              currentHL = el[c];
-              elPosition = elements[i].indexOf(el[c]);
-              elType = el[c]
-                .replace(/(\[|\])/gi, "")
-                .substr(0, el[c].replace(/(\[|\])/gi, "").indexOf(" "));
-              elType = elType.split("").join(" ");
-              elContent = el[c]
-                .replace(/(\[|\])/gi, "")
-                .substr(el[c].replace(/(\[|\])/gi, "").indexOf(" ") + 1);
-              let lineHTML = line.substr(elPosition, currentHL.length);
-              elToLast = currentHL.length + elPosition;
-              lineLength = line.length;
-              hLlength = currentHL.length;
-              finalLine =
-                elements[i].substr(0, elPosition) +
-                `<span class='${elType}'>${elContent}</span>` +
-                elements[i].substr(elToLast, lineLength);
-              elements[i] = finalLine;
+    return new Promise(function(resolve, reject) {
+      if (filePath) {
+        elements = [];
+        let inputFile;
+        getFile(filePath).then(inputFile => {
+          let lines = inputFile.split("\n");
+          let arrayOfLines = [];
+          for (var i = 0; i < lines.length; i++) {
+            if (/^#/.test(lines[i])) {
+              let lastIndicator = lines[i];
+              arrayOfLines.push(lastIndicator);
+              continue;
             }
-
-            el = elements[i].match(matchesLinks);
-            for (var g = 0; g < el.length; g++) {
-              line = elements[i];
-              lineLength = line.length;
-              elPosition = elements[i].indexOf(el[g]);
-              currentLink = el[g];
-              linkText = currentLink
-                .match(matchesLinkText)[0]
-                .replace(/(\[a\s|\])/gi, "");
-              linkAdd = currentLink
-                .match(matchesLinkAdd)[0]
-                .replace(/(\(|\))/gi, "");
-              elToLast = currentLink.length + elPosition;
-              finalLine =
-                elements[i].substr(0, elPosition) +
-                `<a target="_blank" href="${linkAdd}">${linkText}</a>` +
-                elements[i].substr(elToLast, lineLength);
-              elements[i] = finalLine;
+            if (/\n\r/.test(lines[i])) {
+              continue;
             }
-          } else if (hl) {
-            let line = elements[i];
-            let finalLine;
-            el = elements[i].match(matchesHL);
-            for (var c = 0; c < el.length; c++) {
-              currentHL = el[c];
-              elPosition = elements[i].indexOf(el[c]);
-              elType = el[c]
-                .replace(/(\[|\])/gi, "")
-                .substr(0, el[c].replace(/(\[|\])/gi, "").indexOf(" "));
-              elType = elType.split("").join(" ");
-              elContent = el[c]
-                .replace(/(\[|\])/gi, "")
-                .substr(el[c].replace(/(\[|\])/gi, "").indexOf(" ") + 1);
-              let lineHTML = line.substr(elPosition, currentHL.length);
-              elToLast = currentHL.length + elPosition;
-              lineLength = line.length;
-              hLlength = currentHL.length;
-              finalLine =
-                elements[i].substr(0, elPosition) +
-                `<span class='${elType}'>${elContent}</span>` +
-                elements[i].substr(elToLast, lineLength);
-              elements[i] = finalLine;
+            if (/^\s/.test(lines[i]) || lines[i] == "") {
+              continue;
             }
-          } else if (li) {
-            el = elements[i].match(matchesLinks);
-            for (var g = 0; g < el.length; g++) {
-              line = elements[i];
-              lineLength = line.length;
-              elPosition = elements[i].indexOf(el[g]);
-              currentLink = el[g];
-              linkText = currentLink
-                .match(matchesLinkText)[0]
-                .replace(/(\[a\s|\])/gi, "");
-              linkAdd = currentLink
-                .match(matchesLinkAdd)[0]
-                .replace(/(\(|\))/gi, "");
-              elToLast = currentLink.length + elPosition;
-              finalLine =
-                elements[i].substr(0, elPosition) +
-                `<a target="_blank" href="${linkAdd}">${linkText}</a>` +
-                elements[i].substr(elToLast, lineLength);
-              elements[i] = finalLine;
+            if (/^\/\//.test(lines[i])) {
+              continue;
+            } else {
+              arrayOfLines[arrayOfLines.length - 1] = arrayOfLines[
+                arrayOfLines.length - 1
+              ] += "<br>" + lines[i];
+              continue;
             }
           }
-        }
+          for (var i = 0; i < arrayOfLines.length; i++) {
+            lineText = arrayOfLines[i].split(/^#[a-z]*[0-9]*\s/)[1];
+            lineType = arrayOfLines[i].split(/\s/)[0].split("#")[1];
+            line = arrayOfLines[i];
 
-        for (var i = 0; i < elements.length; i++) {
-          elements[i] = elements[i].replace(matchesEsc, "[");
-        }
+            switch (lineType) {
+              case "h1":
+              case "h2":
+              case "h3":
+              case "h4":
+              case "h5":
+              case "h6":
+              case "p":
+              case "code":
+                c = `<${lineType}>${lineText}</${lineType}>`;
+                elements.push(c);
+                break;
+              case "img":
+                x = lineText.split(/(^.*\/\S*)/);
+                imgPath = x[1];
+                imgAlt = x[2].trim();
+                c = `<${lineType} src="${imgPath}" alt="${imgAlt}" title="${imgAlt}">`;
+                elements.push(c);
+                break;
+              default:
+            }
+          }
+          const matchesHL = /(\[[^\\a].*?\])/gi;
+          const matchesLinks = /(\[[a].*?\]\(.*?\))/gi;
+          const matchesEsc = /(\[\\)/gi;
+          const matchesLinkText = /(\[[a].*?\])/;
+          const matchesLinkAdd = /(\(.*?\))/;
+          for (var i = 0; i < elements.length; i++) {
+            let hl = elements[i].match(matchesHL);
+            let li = elements[i].match(matchesLinks);
 
-        for (var i = 0; i < elements.length; i++) {
-          element = elements[i];
-          index = i;
-          elements[i] = {
-            [index]: element
-          };
-        }
-      });
-      return elements;
-    } else {
-      console.warn("no file path specified.");
-      return false;
-    }
+            if (li && hl) {
+              let line = elements[i];
+              let finalLine;
+              el = elements[i].match(matchesHL);
+              for (var c = 0; c < el.length; c++) {
+                currentHL = el[c];
+                elPosition = elements[i].indexOf(el[c]);
+                elType = el[c]
+                  .replace(/(\[|\])/gi, "")
+                  .substr(0, el[c].replace(/(\[|\])/gi, "").indexOf(" "));
+                elType = elType.split("").join(" ");
+                elContent = el[c]
+                  .replace(/(\[|\])/gi, "")
+                  .substr(el[c].replace(/(\[|\])/gi, "").indexOf(" ") + 1);
+                let lineHTML = line.substr(elPosition, currentHL.length);
+                elToLast = currentHL.length + elPosition;
+                lineLength = line.length;
+                hLlength = currentHL.length;
+                finalLine =
+                  elements[i].substr(0, elPosition) +
+                  `<span class='${elType}'>${elContent}</span>` +
+                  elements[i].substr(elToLast, lineLength);
+                elements[i] = finalLine;
+              }
+
+              el = elements[i].match(matchesLinks);
+              for (var g = 0; g < el.length; g++) {
+                line = elements[i];
+                lineLength = line.length;
+                elPosition = elements[i].indexOf(el[g]);
+                currentLink = el[g];
+                linkText = currentLink
+                  .match(matchesLinkText)[0]
+                  .replace(/(\[a\s|\])/gi, "");
+                linkAdd = currentLink
+                  .match(matchesLinkAdd)[0]
+                  .replace(/(\(|\))/gi, "");
+                elToLast = currentLink.length + elPosition;
+                finalLine =
+                  elements[i].substr(0, elPosition) +
+                  `<a target="_blank" href="${linkAdd}">${linkText}</a>` +
+                  elements[i].substr(elToLast, lineLength);
+                elements[i] = finalLine;
+              }
+            } else if (hl) {
+              let line = elements[i];
+              let finalLine;
+              el = elements[i].match(matchesHL);
+              for (var c = 0; c < el.length; c++) {
+                currentHL = el[c];
+                elPosition = elements[i].indexOf(el[c]);
+                elType = el[c]
+                  .replace(/(\[|\])/gi, "")
+                  .substr(0, el[c].replace(/(\[|\])/gi, "").indexOf(" "));
+                elType = elType.split("").join(" ");
+                elContent = el[c]
+                  .replace(/(\[|\])/gi, "")
+                  .substr(el[c].replace(/(\[|\])/gi, "").indexOf(" ") + 1);
+                let lineHTML = line.substr(elPosition, currentHL.length);
+                elToLast = currentHL.length + elPosition;
+                lineLength = line.length;
+                hLlength = currentHL.length;
+                finalLine =
+                  elements[i].substr(0, elPosition) +
+                  `<span class='${elType}'>${elContent}</span>` +
+                  elements[i].substr(elToLast, lineLength);
+                elements[i] = finalLine;
+              }
+            } else if (li) {
+              el = elements[i].match(matchesLinks);
+              for (var g = 0; g < el.length; g++) {
+                line = elements[i];
+                lineLength = line.length;
+                elPosition = elements[i].indexOf(el[g]);
+                currentLink = el[g];
+                linkText = currentLink
+                  .match(matchesLinkText)[0]
+                  .replace(/(\[a\s|\])/gi, "");
+                linkAdd = currentLink
+                  .match(matchesLinkAdd)[0]
+                  .replace(/(\(|\))/gi, "");
+                elToLast = currentLink.length + elPosition;
+                finalLine =
+                  elements[i].substr(0, elPosition) +
+                  `<a target="_blank" href="${linkAdd}">${linkText}</a>` +
+                  elements[i].substr(elToLast, lineLength);
+                elements[i] = finalLine;
+              }
+            }
+          }
+
+          for (var i = 0; i < elements.length; i++) {
+            elements[i] = elements[i].replace(matchesEsc, "[");
+          }
+
+          for (var i = 0; i < elements.length; i++) {
+            element = elements[i];
+            index = i;
+            elements[i] = {
+              [index]: element
+            };
+          }
+          resolve(elements);
+        });
+        return elements;
+      } else {
+        console.warn("no file path specified.");
+        reject("no file path specified");
+      }
+    });
   },
 
   decon: html => {
@@ -254,6 +259,7 @@ const markzilla = (mkz = {
       let date = Object.keys(postObject)[0];
       let body = Object.values(postObject)[0].body;
       let parsedDate = parseDateString(date);
+      // stuff that needs to be changed
       document.querySelector(".mkzTitle").innerHTML = title;
       document.querySelector(".mkzDate").textContent =
         parsedDate.y + " " + parsedDate.m + " " + parsedDate.d + " ";
@@ -265,6 +271,7 @@ const markzilla = (mkz = {
           continue;
         }
       }
+      // end stuff that needs to be changed
       let end = window.performance.now();
       let time =
         "completed task in " + Math.floor(end - start) * 0.001 + " seconds.";
@@ -276,44 +283,37 @@ const markzilla = (mkz = {
   },
 
   saveToPost: json => {
-    if (json.length !== 0) {
-      articleDate = yyyymmdd().yyyymmdd;
-      articleTitle = Object.values(json[0])[0];
-      postObject = {
-        [articleDate]: {
-          title: articleTitle,
-          body: []
+    return new Promise(function(resolve, reject) {
+      if (json.length !== 0) {
+        articleDate = yyyymmdd().yyyymmdd;
+        articleTitle = Object.values(json[0])[0];
+        postObject = {
+          [articleDate]: {
+            title: articleTitle,
+            body: []
+          }
+        };
+        for (var i = 1; i < json.length; i++) {
+          postObject[articleDate].body[i] = Object.values(json[i])[0];
         }
-      };
-      for (var i = 1; i < json.length; i++) {
-        postObject[articleDate].body[i] = Object.values(json[i])[0];
+        resolve(postObject);
+      } else {
+        console.warn("no json to save.");
+        reject("no json to save");
       }
-      return postObject;
-    } else {
-      console.warn("no json to save.");
-      return false;
-    }
+    });
   },
 
   createPost: filePath => {
-    let start = window.performance.now();
-    markzilla.parse(filePath);
-
-    const elementsready = () => {
-      return elements.length != 0;
-    };
-
-    const returnelements = () => {
-      markzilla.saveToPost(elements);
-      markzilla.insertPost(postObject);
-    };
-
-    when(elementsready, returnelements, 2);
-
-    let end = window.performance.now();
-    let time =
-      "completed task in " + Math.ceil(end - start) * 0.001 + " seconds.";
-    return time, postObject;
+    return new Promise(function(resolve, reject) {
+      if (filePath) {
+        markzilla.parse(filePath).then(k => {
+          markzilla.saveToPost(k).then(v => {
+            resolve(v);
+          });
+        });
+      } else reject(new Error("no filepath"));
+    });
   }
 });
 // immediately takes the .css and .mkz
